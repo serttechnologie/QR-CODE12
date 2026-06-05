c6923ea090575005d30a1ff0eb3b9e5012e50d69
serttechnologie <sert.technologie@gmail.com>
2026-06-05 20:44:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  DRIVE_CLIENT_ID: '510292526417-007bs9oh8tdif3hgkr4ov33ns7ctl5kq.apps.googleusercontent.com',
+  DRIVE_SCOPES: 'https://www.googleapis.com/auth/drive.file',
+  DRIVE_FILE_NAME: 'qr-pro-backup.json'
+};
